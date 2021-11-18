660b2171e20f76ba6d659b95af1696b3f2882f17
Rafael Lemos <rhplemos@gmail.com>
2021-11-18 12:12:22 -0300

    Initial commit

---
+++ src/main/java/expertostech/tutorial/rest/api/controller/StatusController.java
@@ -0,0 +1,16 @@
+//este controler verificar se a aplicação está online
+
+package expertostech.tutorial.rest.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping(path = "/api/status")
+    public String check() {
+
+        return "online";
+    }
+}
